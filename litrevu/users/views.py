02e87d644c779b8e2c8f09de5d19c3d04b3d2f33
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-09-18 22:23:33 +0200

    feat: add authentication logic and user feedback messages

--- litrevu/users/views.py
+++ litrevu/users/views.py
@@ -1,13 +1,24 @@
+from django.contrib.auth import login
+from django.contrib.auth import authenticate
 from django.shortcuts import render
 from users import forms
 
 
 def login_page(request):
     form = forms.LoginForm()
+    message = ''
     if request.method == 'POST':
         form = forms.LoginForm(request.POST)
         if form.is_valid():
-            pass
+            user = authenticate(
+                username = form.cleaned_data["username"],
+                password = form.cleaned_data["password"]
+            )
+            if user is not None:
+                login(request, user)
+                message = f"Hello, {user.username}! You are logged in."
+            else:
+                message = f"Invalid credentials."
 
-    return render(request, 'users/login.html', {"form": form})
-
+    return render(request, 'users/login.html',
+                  {"form": form, 'message': message})
