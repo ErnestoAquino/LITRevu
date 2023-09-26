2fd3f124f2dce30336cc45c88eac681898f81c14
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-09-26 14:58:10 +0200

    feature(models): Add foreign key to Ticket model

--- litrevu/feed/models.py
+++ litrevu/feed/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-# from django.conf import settings
+from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.core.validators import MaxValueValidator
 
@@ -9,7 +9,7 @@ from users.models import CustomUser
 class Ticket(models.Model):
     title = models.fields.CharField(max_length = 128)
     description = models.fields.TextField(max_length = 2048, blank = True)
-    # user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
     image = models.ImageField(null = True, blank = True)
     time_create = models.DateTimeField(auto_now_add = True)
 
