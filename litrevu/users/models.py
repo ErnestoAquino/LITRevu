cb4c058c3a8d86eeaa98b67738135107a775c4d2
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-09-26 13:02:00 +0200

    FEED: Created UserFollows model in models.py.

--- litrevu/users/models.py
+++ litrevu/users/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import UserManager
+from django.db import models
+from django.conf import settings
 
 
 class CustomUser(AbstractUser):
@@ -7,3 +9,19 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class UserFollows(models.Model):
+    user: CustomUser = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                         on_delete = models.CASCADE,
+                                         related_name = 'following')
+
+    followed_user: CustomUser = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                                  on_delete = models.CASCADE,
+                                                  related_name = 'followed_by')
+
+    class Meta:
+        unique_together = ('user', 'followed_user')
+
+    def __str__(self):
+        return f"{self.user.username} follows {self.followed_user.username}"
