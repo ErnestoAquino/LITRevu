be3b7e0dcfe43f8c49fe10201b6acbfd81748423
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-09-29 12:30:38 +0200

    [FEED] Enhance admin interface for Ticket and Review

--- litrevu/feed/admin.py
+++ litrevu/feed/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from feed.models import Ticket
 from feed.models import Review
 
-admin.site.register(Ticket)
-admin.site.register(Review)
+
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user')
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('headline', 'ticket')
+
+
+admin.site.register(Ticket, TicketAdmin)
+admin.site.register(Review, ReviewAdmin)
