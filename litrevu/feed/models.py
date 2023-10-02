2682cf37f7049fc152f9d3375312e3714d475350
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-10-02 11:44:31 +0200

    refactor: clean up ticket update functionality

--- litrevu/feed/models.py
+++ litrevu/feed/models.py
@@ -10,7 +10,7 @@ class Ticket(models.Model):
     title = models.fields.CharField(max_length = 128)
     description = models.fields.TextField(max_length = 2048, blank = True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
-    image = models.ImageField(null = True, blank = True)
+    image = models.ImageField(null = True, blank = True, upload_to = 'tickets/')
     time_create = models.DateTimeField(auto_now_add = True)
 
     def __str__(self):
