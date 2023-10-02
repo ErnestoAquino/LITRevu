848bcb2e0fbe6c793b8ed887237e8cfff7d3264c
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-10-02 11:55:14 +0200

    feat: configure MEDIA_URL and MEDIA_ROOT for image handling

--- litrevu/litrevu/settings.py
+++ litrevu/litrevu/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,3 +129,6 @@ AUTH_USER_MODEL = 'users.CustomUser'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = LOGIN_URL
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
