a0450d29a81839bdfb8b8a97efef15a8c6c59c02
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-10-02 11:53:44 +0200

    refactor: clean up ticket update functionality

--- litrevu/feed/views.py
+++ litrevu/feed/views.py
@@ -1,10 +1,16 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 from django.views.generic import View
 from django.views.generic.edit import FormView
+from django.views.generic.edit import UpdateView
 from feed.forms import TicketForm
+from feed.models import Ticket
 
 
 @method_decorator(login_required, name = "dispatch")
@@ -12,7 +18,8 @@ class HomePage(View):
     template_name = "feed/home.html"
 
     def get(self, request):
-        return render(request, self.template_name)
+        user_tickets = Ticket.objects.filter(user = request.user).order_by('-time_create')
+        return render(request, self.template_name, {"user_tickets": user_tickets})
 
 
 @method_decorator(login_required, name = "dispatch")
@@ -25,4 +32,20 @@ class TicketCreatePage(FormView):
         ticket = form.save(commit = False)
         ticket.user = self.request.user
         ticket.save()
+        return super().form_valid(form)
+
+
+class TicketUpdateView(LoginRequiredMixin, UpdateView):
+    model = Ticket
+    form_class = TicketForm
+    template_name = "feed/ticket_update.html"
+    success_url = reverse_lazy("home")
+
+    def get_object(self, queryset=None):
+        ticket = super().get_object(queryset)
+        if ticket.user != self.request.user:
+            raise PermissionDenied("You do not have permission to edit this ticket.")
+        return ticket
+
+    def form_valid(self, form):
         return super().form_valid(form)
