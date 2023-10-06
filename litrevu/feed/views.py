5e1ae86ecd12c9d316fd5feeb7ce8f8f1221043f
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-10-06 14:20:48 +0200

    refactor: rename class 'HomePage' to 'FeedView'

--- litrevu/feed/views.py
+++ litrevu/feed/views.py
@@ -4,32 +4,49 @@ from django.core.exceptions import PermissionDenied
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.shortcuts import render
-# from django.shortcuts import get_object_or_404
-# from django.shortcuts import redirect
 from django.views.generic import View
 from django.views.generic.edit import FormView
 from django.views.generic.edit import UpdateView
 from django.views.generic.edit import DeleteView
+from django.db.models import CharField
+from django.db.models import Value
+from itertools import chain
 from feed.forms import TicketForm
 from feed.forms import ReviewForm
 from feed.models import Ticket
 from feed.models import Review
 
 
-@method_decorator(login_required, name = "dispatch")
-class HomePage(View):
-    template_name = "feed/home.html"
+class FeedView(LoginRequiredMixin, View):
+    def get_users_viewable_reviews(self, user):
+        followed_users = user.following.values_list('followed_user', flat = True)
+        return Review.objects.filter(user_id__in = followed_users)
 
-    def get(self, request):
-        user_tickets = Ticket.objects.filter(user = request.user).order_by('-time_create')
-        return render(request, self.template_name, {"user_tickets": user_tickets})
+    def get_users_viewable_tickets(self, user):
+        followed_users = user.following.values_list('followed_user', flat = True)
+        return Ticket.objects.filter(user_id__in = followed_users)
+
+    def get(self, request, *args, **kwargs):
+        reviews = self.get_users_viewable_reviews(request.user)
+        reviews = reviews.annotate(content_type = Value('REVIEW', CharField()))
+
+        tickets = self.get_users_viewable_tickets(request.user)
+        tickets = tickets.annotate(content_type = Value('TICKET', CharField()))
+
+        posts = sorted(
+            chain(reviews, tickets),
+            key = lambda post: post.time_create,
+            reverse = True
+        )
+
+        return render(request, 'feed/feed.html', context = {'posts': posts})
 
 
 @method_decorator(login_required, name = "dispatch")
 class TicketCreatePage(FormView):
     template_name = "feed/ticket_create.html"
     form_class = TicketForm
-    success_url = reverse_lazy("home")
+    success_url = reverse_lazy("feed")
 
     def form_valid(self, form):
         ticket = form.save(commit = False)
@@ -57,7 +74,7 @@ class TicketUpdateView(LoginRequiredMixin, UpdateView):
 class TicketDeleteView(LoginRequiredMixin, DeleteView):
     model = Ticket
     template_name = "feed/ticket_delete.html"
-    success_url = reverse_lazy("home")
+    success_url = reverse_lazy("posts")
 
     def get_object(self, queryset = None):
         ticket = super().get_object(queryset)
@@ -66,12 +83,12 @@ class TicketDeleteView(LoginRequiredMixin, DeleteView):
         return ticket
 
 
-@method_decorator(login_required, name='dispatch')
+@method_decorator(login_required, name = 'dispatch')
 class CreateTicketAndReviewView(FormView):
     template_name = 'feed/ticket_and_review_create.html'
     form_class = TicketForm  # esto será accessible como 'form' en el template
     second_form_class = ReviewForm  # se tiene que manejar manualmente en la vista
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('feed')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -80,13 +97,13 @@ class CreateTicketAndReviewView(FormView):
         return context
 
     def form_valid(self, form):
-        ticket = form.save(commit=False)
+        ticket = form.save(commit = False)
         ticket.user = self.request.user
         ticket.save()
 
         form2 = self.second_form_class(self.request.POST)
         if form2.is_valid():
-            review = form2.save(commit=False)
+            review = form2.save(commit = False)
             review.ticket = ticket
             review.user = self.request.user
             review.save()
@@ -95,13 +112,13 @@ class CreateTicketAndReviewView(FormView):
             return self.form_invalid(form)
 
 
-@method_decorator(login_required, name='dispatch')
+@method_decorator(login_required, name = 'dispatch')
 class PostView(View):
     template_name = "feed/posts.html"
 
     def get(self, request):
-        user_tickets = Ticket.objects.filter(user=request.user).order_by('-time_create')
-        user_reviews = Review.objects.filter(user=request.user).order_by('-time_create')
+        user_tickets = Ticket.objects.filter(user = request.user).order_by('-time_create')
+        user_reviews = Review.objects.filter(user = request.user).order_by('-time_create')
         return render(request, self.template_name, {"user_tickets": user_tickets, "user_reviews": user_reviews})
 
 
@@ -111,7 +128,7 @@ class ReviewUpdateView(LoginRequiredMixin, UpdateView):
     template_name = "feed/review_update.html"
     success_url = reverse_lazy("posts")
 
-    def get_object(self, queryset=None):
+    def get_object(self, queryset = None):
         review = super().get_object(queryset)
         if review.ticket.user != self.request.user:
             raise PermissionDenied("You do not have permission to edit this review")
@@ -126,3 +143,21 @@ class ReviewUpdateView(LoginRequiredMixin, UpdateView):
         review = self.get_object()
         context['ticket'] = review.ticket
         return context
+
+
+class ReviewDeleteView(LoginRequiredMixin, DeleteView):
+    """
+    View to handle the deletion of a Review instance.
+
+    This view ensures that only the user who created a review can delete it,
+    redirecting to the 'posts' page upon successful deletion.
+    """
+    model = Review
+    template_name = "feed/review_delete.html"
+    success_url = reverse_lazy("posts")
+
+    def get_object(self, queryset = None):
+        review = super().get_object(queryset)
+        if review.user != self.request.user:
+            raise PermissionDenied("You do not have permission to delete this review")
+        return review
